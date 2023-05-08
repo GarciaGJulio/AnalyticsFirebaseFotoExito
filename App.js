288776e52734accc0dd9fd736ec2555b8f553e70
AntonioVillegas13 <antonio.villegas@epn.edu.ec>
2023-05-08 11:48:43 -0500

    df

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import TarjPercha from './app/components/TarjetaPercha';
+import { useState } from 'react';
+import TarjPromo from './app/components/TarjetaPromo';
 
 export default function App() {
-
+  const [valueGeneral, setValueGeneral] = useState();
+  const [valueModerna, setValueModerna] = useState();
+  const [checked, setChecked] = useState(false);
 
   return (
     <View style={styles.container}>
 
-    
+      <TarjPromo exhibidorNombre={"Exhibidor 1"} onChangeStatus={setChecked} />
+
+      <TarjPercha categoriaNombre={"PAN"} oncha onChangeTextModerna={setValueModerna} onChangeTextGeneral={setValueGeneral} />
+      <Text>General:{valueGeneral}</Text>
+      <Text>Moderna:{valueModerna}</Text>
+      <Text>Status:{ checked ? "si" : "no"}</Text>
 
       <StatusBar style="auto" />
     </View>
